89a06a637041d3b947857ccc6c58e3738a134efe
Christopher-0118 <k.karantsevich@gmail.com>
2018-03-14 19:26:25 +0300

    longest-consecutive-sequence/feature: implement longestConsecutiveLength method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+function getConsecutiveLength(set, iterator) {
+  let value = iterator;
+  let consecutiveLength = 1;
+  
+  while (set.has(value + 1)) {
+    consecutiveLength++;
+    value++;
+  }
+  return consecutiveLength;
+}
+
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let set = new Set(array);
+  let setSize = set.size;
+  let sequenceLength = 0;
+  let consecutiveLength;
+  
+  if (setSize === 0 || setSize === 1) {
+    return setSize;
+  }
+  for (let iterator of set.values()) {
+    if (!set.has(iterator - 1)) {
+      consecutiveLength = getConsecutiveLength(set, iterator);
+      if (sequenceLength < consecutiveLength) {
+        sequenceLength = consecutiveLength;
+      }
+    }  
+  }
+  return sequenceLength;
 }
